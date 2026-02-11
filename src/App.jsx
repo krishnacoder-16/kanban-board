41b738df941e308f86746327a6ca6aca570473e7
Krishna Kumar <krishna160504@gmail.com>
2026-02-11 13:48:58 +0530

    Initial setup: Vite + React + Tailwind configured

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Kanban Task Board
+      </h1>
+
+      <div className="flex flex-col md:flex-row gap-6 justify-center">
+        {/* Columns will go here */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
